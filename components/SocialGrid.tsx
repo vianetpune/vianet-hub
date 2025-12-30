366830c9bbb4f04b0e0baea19e5bba8833bf27fb
vianetpune <vianet.sales@gmail.com>
2025-12-30 10:44:57 +0530

    Create SocialGrid.tsx

---
+++ components/SocialGrid.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SocialGrid: React.FC = () => (
+  <div className="social-grid py-20 text-center"><h2 className="text-3xl font-bold mb-4">Connect on Social Media</h2></div>
+);
+
+export default SocialGrid;
